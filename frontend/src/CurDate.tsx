a1da5dce3d31eb89c9e93683c730ac71fcfc0d0d
hyungsuk0315 <hyungsuk0315@naver.com>
2021-01-25 00:35:09 +0900

    Git pull-resquest test#2
    Signed-off-by: hyungsuk0315 <hyungsuk0315@naver.com>

--- frontend/src/CurDate.tsx
+++ frontend/src/CurDate.tsx
@@ -5,7 +5,7 @@ interface IProps {
 
 const CurDate: React.FC<IProps> = (props: IProps) => 
     <div id="CurDate">
-        <h1>This is CurDate haha</h1>
+        <h1>This is CurDate hahaha</h1>
     </div>
 
 CurDate.defaultProps = {
