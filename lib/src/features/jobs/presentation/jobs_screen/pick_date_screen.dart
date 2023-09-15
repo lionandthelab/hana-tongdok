c831ec898ff98ae0f4a283208830587e7ef72f53
kevin <hyungsuk0315@naver.com>
2023-09-15 17:25:00 +0900

    add calendar

--- lib/src/features/jobs/presentation/jobs_screen/pick_date_screen.dart
+++ lib/src/features/jobs/presentation/jobs_screen/pick_date_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:starter_architecture_flutter_firebase/src/common_widgets/date_time_picker.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
+    show CalendarCarousel;
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/classes/event_list.dart';
+import 'package:intl/intl.dart' show DateFormat;
 
 
 class HomePage extends StatefulWidget {
@@ -9,21 +15,91 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPage=0;
+  DateTime _currentDate = DateTime.now();
+  DateTime _currentDate2 = DateTime.now();
+  String _currentMonth = DateFormat.yMMM().format(DateTime.now());
+  DateTime _targetDateTime = DateTime.now();
+//  List<DateTime> _markedDate = [DateTime(2018, 9, 20), DateTime(2018, 10, 11)];
+  static Widget _eventIcon = new Container(
+    decoration: new BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(1000)),
+        border: Border.all(color: Colors.blue, width: 2.0)),
+    child: new Icon(
+      Icons.person,
+      color: Colors.amber,
+    ),
+  );
 
-  final PageController controller = PageController(viewportFraction: 0.8, keepPage: true);
-  void initState() {
-    super.initState();
+  EventList<Event> _markedDateMap = new EventList<Event>(
+    events: {
+      new DateTime(2019, 2, 10): [
+        new Event(
+          date: new DateTime(2019, 2, 10),
+          title: 'Event 1',
+          icon: _eventIcon,
+          dot: Container(
+            margin: EdgeInsets.symmetric(horizontal: 1.0),
+            color: Colors.red,
+            height: 5.0,
+            width: 5.0,
+          ),
+        ),
+        new Event(
+          date: new DateTime(2019, 2, 10),
+          title: 'Event 2',
+          icon: _eventIcon,
+        ),
+        new Event(
+          date: new DateTime(2019, 2, 10),
+          title: 'Event 3',
+          icon: _eventIcon,
+        ),
+      ],
+    },
+  );
 
-    controller.addListener(() {
-      // 페이지 컨트롤러의 상태가 변경될 때 실행할 코드 작성
-      print('현재 페이지: ${controller.page}');
-    });
-  }
   @override
-  void dispose() {
-    controller.dispose(); // 페이지 컨트롤러를 해제해야 합니다.
-    super.dispose();
+  void initState() {
+    /// Add more events to _markedDateMap EventList
+    _markedDateMap.add(
+        new DateTime(2019, 2, 25),
+        new Event(
+          date: new DateTime(2019, 2, 25),
+          title: 'Event 5',
+          icon: _eventIcon,
+        ));
+
+    _markedDateMap.add(
+        new DateTime(2019, 2, 10),
+        new Event(
+          date: new DateTime(2019, 2, 10),
+          title: 'Event 4',
+          icon: _eventIcon,
+        ));
+
+    _markedDateMap.addAll(new DateTime(2019, 2, 11), [
+      new Event(
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 1',
+        icon: _eventIcon,
+      ),
+      new Event(
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 2',
+        icon: _eventIcon,
+      ),
+      new Event(
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 3',
+        icon: _eventIcon,
+      ),
+    ]);
+    super.initState();
   }
+  final PageController controller = PageController(viewportFraction: 0.8, keepPage: true);
+
 
   final idx = 4;
   @override
@@ -54,12 +130,78 @@ class _HomePageState extends State<HomePage> {
       child: Container(
         height: MediaQuery.of(context).size.height,
         child: Center(
-            child: Text(
-              "HOMEHOME",
-              style: TextStyle(color: Colors.indigo),
-            )),
+            child: DateTimePicker(
+              labelText: 'date',
+              selectedDate: DateTime.now(),
+              selectedTime: TimeOfDay.now(),
+            )
+        ),
         )
       )
+    );
+
+
+
+    /// Example Calendar Carousel without header and custom prev & next button
+    final _calendarCarouselNoHeader = CalendarCarousel<Event>(
+      todayBorderColor: Colors.green,
+      onDayPressed: (date, events) {
+        this.setState(() => _currentDate2 = date);
+        events.forEach((event) => print(event.title));
+      },
+      daysHaveCircularBorder: true,
+      showOnlyCurrentMonthDate: false,
+      weekendTextStyle: TextStyle(
+        color: Colors.red,
+      ),
+      thisMonthDayBorderColor: Colors.grey,
+      weekFormat: false,
+//      firstDayOfWeek: 4,
+      markedDatesMap: _markedDateMap,
+      height: 420.0,
+      selectedDateTime: _currentDate2,
+      targetDateTime: _targetDateTime,
+      customGridViewPhysics: NeverScrollableScrollPhysics(),
+      markedDateCustomShapeBorder:
+      CircleBorder(side: BorderSide(color: Colors.yellow)),
+      markedDateCustomTextStyle: TextStyle(
+        fontSize: 18,
+        color: Colors.blue,
+      ),
+      showHeader: false,
+      todayTextStyle: TextStyle(
+        color: Colors.blue,
+      ),
+      // markedDateShowIcon: true,
+      // markedDateIconMaxShown: 2,
+      // markedDateIconBuilder: (event) {
+      //   return event.icon;
+      // },
+      // markedDateMoreShowTotal:
+      //     true,
+      todayButtonColor: Colors.yellow,
+      selectedDayTextStyle: TextStyle(
+        color: Colors.yellow,
+      ),
+      minSelectedDate: _currentDate.subtract(Duration(days: 360)),
+      maxSelectedDate: _currentDate.add(Duration(days: 360)),
+      prevDaysTextStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.pinkAccent,
+      ),
+      inactiveDaysTextStyle: TextStyle(
+        color: Colors.tealAccent,
+        fontSize: 16,
+      ),
+      onCalendarChanged: (DateTime date) {
+        this.setState(() {
+          _targetDateTime = date;
+          _currentMonth = DateFormat.yMMM().format(_targetDateTime);
+        });
+      },
+      onDayLongPressed: (DateTime date) {
+        print('long pressed date $date');
+      },
     );
     return Scaffold(
       body: SafeArea(
@@ -67,11 +209,67 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              //custom icon
+// This trailing comma makes auto-formatting nicer for build methods.
+              //custom icon without header
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30.0,
+                  bottom: 16.0,
+                  left: 16.0,
+                  right: 16.0,
+                ),
+                child: new Row(
+                  children: <Widget>[
+                    TextButton(
+                      child: Text('PREV'),
+                      onPressed: () {
+                        setState(() {
+                          _targetDateTime = DateTime(
+                              _targetDateTime.year, _targetDateTime.month - 1);
+                          _currentMonth =
+                              DateFormat.yMMM().format(_targetDateTime);
+                        });
+                      },
+                    ),
+                    Expanded(
+                        child: Center(
+                          child: Text(
+                            _currentMonth,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24.0,
+                            ),
+                          ),
+                        )),
+                    TextButton(
+                      child: Text('NEXT'),
+                      onPressed: () {
+                        setState(() {
+                          _targetDateTime = DateTime(
+                              _targetDateTime.year, _targetDateTime.month + 1);
+                          _currentMonth =
+                              DateFormat.yMMM().format(_targetDateTime);
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16.0),
+                child: _calendarCarouselNoHeader,
+              ), //
               SizedBox(height: 16),
               SizedBox(
 
                 height: MediaQuery.of(context).size.height - 250,
                 child: PageView.builder(
+                  onPageChanged: (page){
+                    setState(() {
+                      currentPage=page;
+                    });
+                  },
                   controller: controller,
                   // itemCount: pages.length,
                   itemBuilder: (_, index) {
@@ -82,8 +280,8 @@ class _HomePageState extends State<HomePage> {
               SizedBox(height: 16),
               SmoothPageIndicator(
                 controller: controller,
-                count: pages.length,
-                effect: controller.page ==0? const WormEffect(
+                count: pages.length - 1,
+                effect: currentPage==0?const WormEffect(
                   dotHeight: 0,
                   dotWidth: 0,
                   type: WormType.thinUnderground,
