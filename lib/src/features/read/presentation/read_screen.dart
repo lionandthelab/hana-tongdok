b0468ac19c93d4cae6b3f119cbde033b819bce0c
Issac Kim <mysmax@Mysmaxs-MacBook-Pro.local>
2023-09-28 01:29:34 +0900

    mark calendar (not completed)
    Signed-off-by: Issac Kim <mysmax@Mysmaxs-MacBook-Pro.local>

--- lib/src/features/read/presentation/read_screen.dart
+++ lib/src/features/read/presentation/read_screen.dart
@@ -5,12 +5,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
+    show CalendarCarousel;
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/classes/event_list.dart';
+import 'package:intl/intl.dart' show DateFormat;
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:starter_architecture_flutter_firebase/src/constants/strings.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/read/data/read_repository.dart';
 import 'package:starter_architecture_flutter_firebase/src/routing/app_router.dart';
 
@@ -22,31 +23,44 @@ class ReadScreen extends StatefulWidget {
 }
 
 class _ReadScreenState extends State<ReadScreen> {
-  late List<dynamic> yourJsonData = [];
+  late List<dynamic> _verseData = [];
   late DateTime? _selectedDate = null;
   late String _verseKey = "";
   double _fontSize = 24.0; // Initial font size
   final today = DateUtils.dateOnly(DateTime.now());
+  late bool _showCalendar = false;
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final ReadRepository readRepository = new ReadRepository();
   CollectionReference product = FirebaseFirestore.instance.collection("users");
 
+  EventList<Event> _markedDateMap = new EventList<Event>(
+    events: {},
+  );
+
   @override
   void initState() {
     super.initState();
-    _selectedDate = DateTime.now();
-    String m = _selectedDate!.month.toString();
-    String d = _selectedDate!.day.toString();
-    _verseKey = "m${m}d$d";
-    //_fontSize = 24.0;
-    _loadJsonData();
+
+    setState(() => {
+          _selectedDate = DateTime.now(),
+          _verseKey = "m${DateTime.now().month}d${DateTime.now().day}",
+        });
+
     _loadUserSettings();
-    _loadDatesData();
     _loadUserDates();
+    print("initState: _verseKey: $_verseKey");
+    _loadJsonData();
   }
 
-  final userSettings = rootBundle.loadString('assets/json/userdata.json');
+  @override
+  void deactivate() async {
+    super.deactivate();
+    print("deactivate: _verseKey: $_verseKey");
+
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setDouble("fontSize", _fontSize);
+  }
 
   void _increaseFontSize() {
     setState(() {
@@ -75,43 +89,54 @@ class _ReadScreenState extends State<ReadScreen> {
     }
   }
 
-  Future<void> _loadDatesData() async {
-    print('hello');
-    final user = FirebaseAuth.instance.currentUser;
-    final path = 'users/${user!.uid}/dates';
+  // Future<void> _loadDatesData() async {
+  //   print('hello');
+  //   final user = FirebaseAuth.instance.currentUser;
+  //   final path = 'users/${user!.uid}/dates';
 
-    if (user != null) {
-      // readRepository.addDate(uid: user.uid, date: "09/13");
-      QuerySnapshot querySnapshot = await _firestore.collection(path).get();
-      print("_loadDatesData: $querySnapshot");
-      querySnapshot.docs.forEach((doc) {
-        print(doc["date"]);
-      });
-    }
-  }
+  //   if (user != null) {
+  //     // readRepository.addDate(uid: user.uid, date: "09/13");
+  //     QuerySnapshot querySnapshot = await _firestore.collection(path).get();
+  //     print("_loadDatesData: $querySnapshot");
+  //     querySnapshot.docs.forEach((doc) {
+  //       print(doc["date"]);
+  //     });
+  //   }
+  // }
 
   Future<void> _loadUserDates() async {
+    print('firestore dates read - start');
     final user = FirebaseAuth.instance.currentUser;
     FirebaseFirestore.instance
         .collection('users')
         .doc(user!.uid)
+        .collection('dates')
         .get()
-        .then((DocumentSnapshot documentSnapshot) {
-      if (documentSnapshot.exists) {
-        print('Document exists on the database');
-      }
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        print(doc["date"]);
+        _markedDateMap.add(
+            DateTime.parse(doc["date"]),
+            new Event(
+              date: DateTime.parse(doc["date"]),
+              icon: Icon(Icons.book_rounded),
+              dot: Container(
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                color: Colors.black,
+                height: 12.0,
+                width: 12.0,
+              ),
+            ));
+      });
     });
-    print('firestore read - ');
+    print('firestore dates read - done ');
   }
 
   Future<void> _loadUserSettings() async {
-    // final jsonString = await rootBundle.loadString('assets/json/userdata.json');
-    // final jsonData = json.decode(jsonString);
     final sharedPreferences = await SharedPreferences.getInstance();
 
     _fontSize = sharedPreferences.getDouble("fontSize") ?? 24.0;
-
-    // _fontSize = jsonData.read_number;
+    print("initState(sharedPreferences): _fontSize: $_fontSize");
   }
 
   Future<void> _loadJsonData() async {
@@ -119,27 +144,15 @@ class _ReadScreenState extends State<ReadScreen> {
         await rootBundle.loadString('assets/json/bible-RNKSV.json');
     final jsonData = json.decode(jsonString);
 
-    // print("@@@jsonData[_verseKey]: $jsonData[_verseKey]");
-
     setState(() {
-      String m = _selectedDate!.month.toString();
-      String d = _selectedDate!.day.toString();
-      _verseKey = "m${m}d$d";
+      // String m = _selectedDate!.month.toString();
+      // String d = _selectedDate!.day.toString();
+      // _verseKey = "m${m}d$d";
 
       // yourJsonData = jsonData[_verseKey]["contents"] as List<Map<String, dynamic>>;
-      yourJsonData = jsonData[_verseKey]["contents"] as List<dynamic>;
-      // print("@@@_bibleList: $_bibleList");
+      _verseData = jsonData[_verseKey]["contents"] as List<dynamic>;
+      print("_verseData: $_verseData");
       // yourJsonData = jsonDecode(_bibleList) as List<Map<String, dynamic>>;
-      // print("yourJsonData: $yourJsonData");
-      print("your read number: $userSettings");
-      // _bibleText = "";
-      // for (int i = 0; i < _bibleList.length; i++) {
-      //   List<dynamic> book = _bibleList[i]["paragraphs"][0];
-      //   for (int j = 0; i < book.length; j++) {
-      //     String verse = book[i]["content"];
-      //     _bibleText += '$verse\n';
-      //   }
-      // }
     });
   }
 
@@ -154,53 +167,148 @@ class _ReadScreenState extends State<ReadScreen> {
     if (pickedDate != null && pickedDate != _selectedDate) {
       setState(() {
         _selectedDate = pickedDate;
-        _loadJsonData();
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.book_rounded),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.calendar_today),
-              onPressed: () => _selectDate(context, [DateTime(2023, 09, 01)]),
-            ),
-            FontSizeAdjusterButton(
-                increaseFontSize: _increaseFontSize,
-                decreaseFontSize: _decreaseFontSize),
-            IconButton(
-              icon: Icon(Icons.history_edu_rounded),
-              onPressed: () => context.goNamed(
-                AppRoute.keep.name,
-                // pathParameters: {'id': proclaim.id},
-              ),
-            ),
-            IconButton(
-              icon: Icon(Icons.check),
-              onPressed: () {
-                if (_selectedDate != null) _submitAddDate(_selectedDate!);
-                print(_selectedDate.toString());
-              },
-            ),
-          ],
+    final _calendarCarousel = CalendarCarousel<Event>(
+      onDayPressed: (date, events) {
+        print("onDayPressed: date: $date");
+        setState(() => {
+              _selectedDate = date,
+              _verseKey = "m${_selectedDate?.month}d${_selectedDate?.day}",
+            });
+
+        _loadJsonData();
+        setState(() => _showCalendar = !_showCalendar);
+      },
+      weekdayTextStyle: TextStyle(
+        color: Colors.black87,
+      ),
+      headerTextStyle: TextStyle(
+        color: Colors.cyan,
+      ),
+      weekendTextStyle: TextStyle(
+        color: Colors.redAccent,
+      ),
+      thisMonthDayBorderColor: Colors.grey,
+//          weekDays: null, /// for pass null when you do not want to render weekDays
+      headerText: '하나통독 캘린더',
+      weekFormat: false,
+      markedDatesMap: _markedDateMap,
+      selectedDateTime: _selectedDate,
+      showIconBehindDayText: true,
+//          daysHaveCircularBorder: false, /// null for not rendering any border, true for circular border, false for rectangular border
+      markedDateShowIcon: true,
+      markedDateIconMaxShown: 2,
+      selectedDayTextStyle: TextStyle(
+        color: Colors.white,
+      ),
+      selectedDayBorderColor: Colors.cyan,
+      selectedDayButtonColor: Colors.cyan,
+      markedDateCustomShapeBorder: BeveledRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      todayTextStyle: TextStyle(
+        color: Colors.cyan,
+      ),
+      markedDateIconBuilder: (event) {
+        return event.icon ?? Icon(Icons.help_outline);
+      },
+      markedDateMoreCustomDecoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.cyanAccent,
+          width: 1.0,
         ),
-        body: StreamBuilder(
-          stream: product.snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-            if (streamSnapshot.hasData) {
-              return TextSizeAdjusterWidget(
-                  jsonData: yourJsonData, fontSize: _fontSize);
-            }
-            return CircularProgressIndicator();
-          },
-        )
-        //,
-        );
+        borderRadius: BorderRadius.circular(4.0),
+        color: Colors.cyanAccent,
+      ),
+      // minSelectedDate: _selectedDate.subtract(Duration(days: 360)),
+      // maxSelectedDate: _selectedDate.add(Duration(days: 360)),
+      customGridViewPhysics: NeverScrollableScrollPhysics(),
+      // markedDateCustomShapeBorder:
+      //     (side: BorderSide(color: Colors.yellow)),
+      markedDateCustomTextStyle: TextStyle(
+        fontSize: 18,
+        color: Colors.blue,
+      ),
+      todayButtonColor: Colors.transparent,
+      todayBorderColor: Colors.cyanAccent,
+      markedDateIconMargin: 5,
+      markedDateIconOffset: 1,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.book_rounded),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.calendar_today),
+            onPressed: () {
+              setState(() => _showCalendar = !_showCalendar);
+              // showDialog(
+              //     context: context,
+              //     barrierDismissible: true, // 바깥 영역 터치시 닫을지 여부
+              //     builder: (BuildContext context) {
+              //       return AlertDialog(
+              //         content: Container(
+              //           margin: EdgeInsets.symmetric(horizontal: 16.0),
+              //           child: _calendarCarousel,
+              //         ), //_calendarCarousel,
+              //         insetPadding: const EdgeInsets.fromLTRB(0, 80, 0, 80),
+              //         actions: [
+              //           TextButton(
+              //             child: const Text('확인'),
+              //             onPressed: () {
+              //               Navigator.of(context).pop();
+              //             },
+              //           ),
+              //         ],
+              //       );
+              //     });
+            },
+            // onPressed: () => _selectDate(context, [DateTime(2023, 09, 01)]),
+          ),
+          FontSizeAdjusterButton(
+              increaseFontSize: _increaseFontSize,
+              decreaseFontSize: _decreaseFontSize),
+          IconButton(
+            icon: Icon(Icons.history_edu_rounded),
+            onPressed: () => context.goNamed(
+              AppRoute.keep.name,
+              // pathParameters: {'id': proclaim.id},
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {
+              if (_selectedDate != null) _submitAddDate(_selectedDate!);
+              print(_selectedDate.toString());
+              _loadUserDates();
+            },
+          ),
+        ],
+      ),
+      body: _showCalendar
+          ? Container(
+              margin: EdgeInsets.symmetric(horizontal: 160.0),
+              child: _calendarCarousel,
+            )
+          : StreamBuilder(
+              stream: product.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                if (streamSnapshot.hasData) {
+                  return TextSizeAdjusterWidget(
+                      jsonData: _verseData, fontSize: _fontSize);
+                }
+                return CircularProgressIndicator();
+              },
+            ), //_cal
+      //,
+    );
   }
 }
 
@@ -359,66 +467,3 @@ class FontSizeAdjusterButton extends StatelessWidget {
     );
   }
 }
-
-
-
-
-// class ReadScreen extends ConsumerWidget {
-//   const ReadScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text(Strings.read),
-//       ),
-//       body: SizedBox(
-//       width: 200.0,
-//       height: 300.0,
-//       child: Scaffold(
-//           body:Column(
-//               children: [
-//                 //Text('asdafs')
-//                 Text(context.watch<DatePickup>().curDate.toString()),
-//                 Text(context.watch<DatePickup>().strDate.toString()),
-//                 CarouselSlider(
-//                   options: CarouselOptions(
-//                       height: 100.0,
-//                       enableInfiniteScroll: false,
-//                   ),
-//                   items: context.watch<DatePickup>().bibleList.map((script) {
-//                     return Builder(
-//                       builder: (BuildContext context) {
-//                         return Container(
-//                             width: MediaQuery.of(context).size.width,
-//                             margin: EdgeInsets.symmetric(horizontal: 5.0),
-//                             decoration: BoxDecoration(
-//                                 color: Colors.amber
-//                             ),
-//                             child: Column(
-//                                 children: [
-//                                   //Text(context.watch<DatePickup>().verses)
-//                                 ]
-//                             )
-//                         );
-//                       },
-//                     );
-//                   }).toList(),
-//                 )
-//               ],
-//             ),
-
-//         ),
-//     )
-//       // body: Swiper(
-//       //   itemBuilder: (BuildContext context, int index){
-//       //     // return Image.network("https://via.placeholder.com/350x150", fit: BoxFit.fill,);
-//       //     return Text(index.toString(), style: TextStyle(_fontSize: 50.0),);
-//       //   },
-//       //   itemCount: 100,
-//       //   pagination: SwiperPagination(),
-//       //   control: SwiperControl(),
-//       // ),
-//     );
-//   }
-// }
